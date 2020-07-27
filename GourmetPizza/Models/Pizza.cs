6d880d73fada509de9daa6d446aaf979a9fff14e
Daniel <daniel.rourke@aussiebroadband.com.au>
2020-07-27 18:12:48 +1000

    Done Prac1

--- GourmetPizza/Models/Pizza.cs
+++ GourmetPizza/Models/Pizza.cs
@@ -10,7 +10,7 @@ namespace GourmetPizza.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public double Price { get; set; }
+        public decimal Price { get; set; }
 
     }
 }
